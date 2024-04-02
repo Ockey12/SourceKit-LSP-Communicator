298747a2dba8d222c6488c05c60970b61506cefb
OnagaHaruki <haloupgarou@gmail.com>
2024-04-03 01:50:54 +0900

    `DidOpen Notification`を送信するFormをViewに追加した

--- SourceKit-LSP-Communicator/SourceKit-LSP-Communicator/ContentView.swift
+++ SourceKit-LSP-Communicator/SourceKit-LSP-Communicator/ContentView.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct ContentView: View {
     private let client = LSPClient()
     @State private var rootPathString = ""
+    @State private var sourceFilePath = ""
+    @State private var sourceCode = ""
 
     var body: some View {
         Form {
@@ -20,6 +22,23 @@ struct ContentView: View {
                     client.sendInitializedNotification()
                 }
             } // Section "Initialization"
+
+            Section(header: Text("DidOpen")) {
+                TextField(
+                    "Source file path",
+                    text: $sourceFilePath
+                )
+
+                TextEditor(text: $sourceCode)
+                    .frame(height: 300)
+
+                Button("Send DidOpen Notification") {
+                    client.sendDidOpenNotification(
+                        sourceFilePathString: sourceFilePath,
+                        sourceCode: sourceCode
+                    )
+                }
+            } // section "DidOpen"
         } // VStack
         .padding()
         .frame(width: 800)
